0dcf952074d007ca87f7101958c70986843bc9a8
enoveri <91349024+enoveri@users.noreply.github.com>
2025-03-17 12:41:18 +0300

    commit

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -23,7 +23,7 @@ function TaskForm({ addTask }) {
         />
         <button
           type="submit"
-          className="flex-shrink-0 bg-indigo-500 hover:bg-indigo-700 border-indigo-500 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
+          className="flex-shrink-0 bg-indigo-500 hover:bg-indigo-700 border-indigo-500 hover:border-indigo-700 text-sm border-4 py-1 px-2 rounded"
         >
           Add
         </button>
